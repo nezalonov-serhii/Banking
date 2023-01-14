28658b3db9ee63c91adb4dcf611ac2752f2dd200
nezalonov-serhii <110600342+nezalonov-serhii@users.noreply.github.com>
2023-01-14 08:55:45 +0000

    Deploying to gh-pages from @ nezalonov-serhii/Banking@4be120fd9378faf3fe9e4814a13d6a6a09f56cd5 🚀

---
+++ index.aa2863c0.js
@@ -0,0 +1,2 @@
+var rootEl=document.querySelector("#root"),listsBanks=document.createElement("div"),descrBank=document.createElement("div");rootEl.append(listsBanks,descrBank),listsBanks.classList.add("listsBanks"),descrBank.classList.add("descrBank");
+//# sourceMappingURL=index.aa2863c0.js.map
