7c2b14a9b3d5e10f087ceb813904cbb2dbcdb241
Sergii <nezalonov.serhii@gmail.com>
2023-01-22 22:03:42 +0200

    add chose bank and render descr

--- src/index.js
+++ src/index.js
@@ -1,52 +1,77 @@
 const banks = [
-    {
-      id: '435tr34wrt',
-      name: 'Mono',
-      interestRate: 5,
-      maxLoan: 500000,
-      minPayment: 1000,
-      loanTerm: 12,
-    },
-    {
-      id: 'asdfw342rew5',
-      name: 'Privat',
-      interestRate: 7,
-      maxLoan: 1000000,
-      minPayment: 5000,
-      loanTerm: 50,
-    },
-  ];
+  {
+    id: '435tr34wrt',
+    name: 'Mono',
+    interestRate: 5,
+    maxLoan: 500000,
+    minPayment: 1000,
+    loanTerm: 12,
+  },
+  {
+    id: 'asdfw342rew5',
+    name: 'Privat',
+    interestRate: 7,
+    maxLoan: 1000000,
+    minPayment: 5000,
+    loanTerm: 50,
+  },
+];
 
 const rootEl = document.querySelector('#root');
 
 const containerBanks = document.createElement('div');
 const descrBank = document.createElement('div');
 const listOfBanks = document.createElement('ul');
-const buttonNewBank = document.createElement("button");
+const buttonNewBank = document.createElement('button');
 
 containerBanks.classList.add('containerBanks');
 descrBank.classList.add('descrBank');
-buttonNewBank.classList.add('buttonNewBank')
-buttonNewBank.textContent = 'Add new Bank'
+listOfBanks.classList.add('list');
+buttonNewBank.classList.add('buttonNewBank');
+buttonNewBank.textContent = 'Add new Bank';
 
 rootEl.append(containerBanks, descrBank);
 containerBanks.append(listOfBanks, buttonNewBank);
 
 function renderBankList() {
-  const bankArray = banks.map(
-    bank => `
-        <li class="first-bank"> ${bank.name}
-     <div>
-               <button>E</button>
-        <button>D</button>
-             </div>
-           </li>
-         `
-  ).join("");
- 
+  const bankArray = banks
+    .map(
+      bank => `
+      <li class="first-bank" data-id = "${bank.id}">
+        <p>${bank.name}</p>
+          <div>
+            <button class = "btn">Edit</button>
+            <button class = "btn btn__delete">Delete</button>
+          </div>
+        </li>
+      `
+    )
+    .join('');
+
   listOfBanks.insertAdjacentHTML('beforeend', bankArray);
 }
 
 renderBankList();
 
+listOfBanks.addEventListener('click', showDescriptionOfBanks);
 
+function showDescriptionOfBanks(e) {
+  if (e.target.nodeName !== 'LI') return;
+
+  const bankId = e.target.closest('.first-bank').dataset.id;
+  console.log(bankId);
+
+  const selectedBank = banks.find(bank => bank.id === bankId);
+  console.log(selectedBank);
+
+  const descriptionOfBank = `
+      <ul class = "list description">
+        <li class = "item-descr" >Bank: ${selectedBank.name}</li>
+        <li class = "item-descr" >Interest Rate: ${selectedBank.interestRate}</li>
+        <li class = "item-descr" >Max Loan: ${selectedBank.maxLoan}</li>
+        <li class = "item-descr" >Min Payment: ${selectedBank.minPayment}</li>
+        <li class = "item-descr" >Loan Term: ${selectedBank.loanTerm}</li>
+      </ul>`;
+
+  descrBank.innerHTML = descriptionOfBank;
+}
